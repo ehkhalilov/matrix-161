da8285433990bc81d989be5017b143701c5a6f69
AydanRahimova <aydanragimova28@gmail.com>
2024-08-17 18:05:34 +0400

    new customer is added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Customer customer = new Customer("Elnur", 29);
+        Customer customer3 = new Customer("Aydan",20);
     }
 }
