c452e21188d92214bfdc8ff40d1678499829a03f
ehkhalilov <elnur.khalilov@pashabank.az>
2024-08-17 17:57:55 +0400

    update main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Customer customer = new Customer("Elnur", 29);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,2 @@
-public class Test str{
+public class Test {
 }
