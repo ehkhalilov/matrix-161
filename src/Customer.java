70fbb5e95091e49e0a2d55d62f83720a3b3810c6
gulayhajiyevaa <gulayhajiyevaa@gmail.com>
2024-08-17 18:09:42 +0400

    soyad

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,11 @@
 public class Customer {
     private String name;
     private int age;
+    private String surname;
 
-    public Customer(String name, int age) {
+    public Customer(String name, int age, String surname) {
         this.name = name;
         this.age = age;
+        this.surname= surname;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Customer customer = new Customer("Elnur", 29);
+        Customer customer = new Customer("Elnur", 30,"Xalilov ");
+
     }
 }
