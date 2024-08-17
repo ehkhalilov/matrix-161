e3fbc45d63181d4779b83570642cb84e8cf71c84
ehkhalilov <elnur.khalilov@pashabank.az>
2024-08-17 18:27:49 +0400

    fix conflicts

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Customer customer = new Customer("Elnur", 29);
-        Customer customer3 = new Customer("Aydan",20);
-        Customer customer = new Customer("Elnur", 30,"Xalilov ");
+        Customer customer = new Customer("Elnur", 29, "");
+        Customer customer3 = new Customer("Aydan", 20, "");
+        Customer customer2 = new Customer("Elnur", 30, "");
 
+        Customer customer4 = new Customer("Aydan", 20, "");
     }
 }
